dce25cdb8361e5a9f6fd273a276df5c9cd7a9666
mzncvmc <mzncvmc@gmail.com>
2022-12-24 22:35:41 +0900

    #5.5 Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { init, exit } from "./myPackage.js";
+import crypto from "crypto";
 
-
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    heigth: number;
+    data: string;
+}
+class Block implements BlockShape{
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public heigth: number,
+        public data: string
+    ){
+        this.hash = Block.calculateHash(prevHash, heigth, data);
+    }
+    static calculateHash(prevHash: string, height: number, data: string){
+        const toHash = `${prevHash}${height}${data}`
+    }
+}
